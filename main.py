588871fe86828dfb534030ab70860f31480a07c5
Colin Thompson <ColinT@gs1ca.org>
2021-01-26 10:02:39 -0500

    old commit

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-def binary_search(list, item):
-  low = 0
-  high = len(list)-1
-
-  while low <= high:
-    mid = (low + high)
-    guess = list[mid]
-    if guess == item:
-      return mid
-    if guess > item:
-      high = mid-1
-    else:
-      low = mid+1
-  return None
-
-my_list = [1, 3, 5, 7, 9]
-
-print(binary_search(my_list, 3))
-print(binary_search(my_list, -1))
+def look_for_key(main_box):
+  pile = main_box.make_a_pile_to_look_through()
+  while pile is not empty:
+    box = pile.grab_a_box()
+    for item in box:
+      if item.is_a_box():
+        pile.append(item)
+      elif item.is_a_key():
+        print("found the key!")
