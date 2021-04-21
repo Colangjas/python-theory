b44ca65da73a78a61da1f11cfe3fbeb8a58f951c
Colin Thompson <colinthompson@pop-os.localdomain>
2021-04-21 16:59:44 -0400

    added recursion examples

--- binary-search.py
+++ binary-search.py
@@ -1,19 +1,4 @@
 def binary_search(list, item):
-<<<<<<< HEAD
-  low = 0
-  high = len(list)-1
-
-  while low <= high:
-    mid = (low + high)
-    guess = list[mid]
-    if guess == item:
-      return mid
-    if guess > item:
-      high = mid-1
-    else:
-      low = mid+1
-  return None
-=======
     low = 0
     high = len(list)-1
 
@@ -28,13 +13,8 @@ def binary_search(list, item):
             low = mid+1
     return None
 
->>>>>>> 9d1e2176c4fea17553bfd28c4fe962ef00b1493c
 
 my_list = [1, 3, 5, 7, 9]
 
 print(binary_search(my_list, 3))
-<<<<<<< HEAD
 print(binary_search(my_list, -1))
-=======
-print(binary_search(my_list, -1))
->>>>>>> 9d1e2176c4fea17553bfd28c4fe962ef00b1493c
--- selection-sort.py
+++ selection-sort.py
@@ -1,22 +1,4 @@
 def findSmallest(arr):
-<<<<<<< HEAD
-  smallest = arr[0]
-  smallest_index = 0
-  for i in range(1,len(arr)):
-    if arr[i] < smallest:
-      smallest = arr[i]
-      smallest_index = i
-  return smallest_index
-
-def selectionSort(arr):
-  newArr = []
-  for i in range(len(arr)):
-    smallest = findSmallest(arr)
-    newArr.append(arr.pop(smallest))
-  return newArr
-
-print(selectionSort([5, 3, 6, 2, 10]))
-=======
     smallest = arr[0]
     smallest_index = 0
     for i in range(1, len(arr)):
@@ -35,4 +17,3 @@ def selectionSort(arr):
 
 
 print(selectionSort([5, 3, 6, 2, 10]))
->>>>>>> 9d1e2176c4fea17553bfd28c4fe962ef00b1493c
