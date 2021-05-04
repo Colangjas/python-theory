1511f328a88af7b05e4ce07be8e457fbdc8057c0
Colin Thompson <thompsoncolinm@gmail.com>
2021-05-04 13:45:58 -0400

    added breadth-first stuff

--- breadth-first/you.py
+++ breadth-first/you.py
@@ -10,20 +10,23 @@ graph["peggy"] = []
 graph["thom"] = []
 graph["jonny"] = []
 
-search_queue = deque()
-search_queue += graph["you"]
 
 
 def person_is_seller(name):
     return name[-1] == 'm'
-def find_mango_seller(search_queue):
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []
     while search_queue:
         person = search_queue.popleft()
-        if person_is_seller(person):
-            print(person + " is a mango seller!")
-            return True
-        else:
-            search_queue += graph[person]
+        if not person in searched:
+            if person_is_seller(person):
+                print(person + " is a mango seller!")
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)
     return False
 
-find_mango_seller(search_queue)
+search("you")
